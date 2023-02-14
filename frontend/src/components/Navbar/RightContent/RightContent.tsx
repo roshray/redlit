14212ffcd70b178b36fa0fbbf7fc29676bafc53b
roshray <roshanroy759@gmail.com>
2023-02-14 22:09:13 +0530

    dropdown[login/logout]:✔️

--- frontend/src/components/Navbar/RightContent/RightContent.tsx
+++ frontend/src/components/Navbar/RightContent/RightContent.tsx
@@ -2,19 +2,28 @@ import React from "react"
 import { Button, Flex } from "@chakra-ui/react"
 import AuthButtons from "./AuthButtons"
 import AuthModal from "@/components/Modal/Auth/AuthModal"
-import { signOut } from "firebase/auth"
-import {auth} from "../../../firebase/clientApp"
+
+import { signOut, User } from "firebase/auth"
+import { auth} from "../../../firebase/clientApp"
+
+import Icons from "./Icons"
+import UserMenu from "./UserMenu"
 
 type RightContentProps = {
-    user: any   
+    user?: User | null
 }
 
 const RightContent:React.FC<RightContentProps> = ({user}) => {    
     return (
         <>
             <AuthModal /> 
-            <Flex justify='center' align='center'>
-                {user ? <Button onClick={() => signOut(auth)}>Logout</Button>:<AuthButtons />}
+            <Flex justify="center" align="center">
+                {user ? ( 
+                    <Icons/>
+                ) : (
+                <AuthButtons />
+                )}
+                <UserMenu user={user}/>  
             </Flex>
         </>
     )
