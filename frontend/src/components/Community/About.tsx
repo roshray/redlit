16720c928167d17c6a92511283a8a6742f04ae0f
roshray <roshanroy759@gmail.com>
2023-03-10 12:35:05 +0530

    imageUpload & update on community logo: ✔️

--- frontend/src/components/Community/About.tsx
+++ frontend/src/components/Community/About.tsx
@@ -1,13 +1,18 @@
-import { Box, Button, Divider, Flex, Icon, Stack, Text } from '@chakra-ui/react';
-import React, { useRef } from 'react';
-import { Community} from "../../atoms/communitiesAtom"
+import { Box, Button, Divider, Flex, Icon, Image, Spinner, Stack, Text } from '@chakra-ui/react';
+import React, { useRef, useState } from 'react';
+import { Community, communityState} from "../../atoms/communitiesAtom"
 import { RiCakeLine} from "react-icons/ri"
 import { HiOutlineDotsHorizontal } from "react-icons/hi";
 import moment from 'moment';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { auth } from '../../firebase/clientApp';
+import { auth, firestore, storage } from '../../firebase/clientApp';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import useSelectFile from '../../hooks/useSelectFile';
+import { FaReddit } from 'react-icons/fa';
+import { getDownloadURL, ref, uploadString } from 'firebase/storage';
+import { doc, updateDoc } from 'firebase/firestore';
+import { useSetRecoilState } from 'recoil';
 
 type AboutProps = {
     communityData: Community;
@@ -15,8 +20,39 @@ type AboutProps = {
 
 const About:React.FC<AboutProps> = ({communityData}) => {
     const [user] = useAuthState(auth)
-    const router = useRouter()
-    const selectedFileRef = useRef<string>() 
+    const selectedFileRef = useRef<HTMLInputElement>(null) 
+    const {selectedFile, setSelectedFile, onSelectFile} = useSelectFile()
+    const [uploadingImage,setUploadingImage] = useState(false)
+    const setCommunityStateValue = useSetRecoilState(communityState)
+
+    const onUpdateImage = async () => {
+        if(!selectedFile) return
+        setUploadingImage(true)
+        try {
+         const imageRef = ref(storage, `communities/${communityData.id}/image`)
+         await uploadString(imageRef, selectedFile, "data_url")
+         const downloadURL = await getDownloadURL(imageRef)
+         await updateDoc(doc(firestore, "communities", communityData.id), {
+            imageURL: downloadURL,
+            
+         })
+
+         setCommunityStateValue(prev => ({
+            ...prev,
+            currentCommunity: {
+                ...prev.currentCommunity,
+                imageURL: downloadURL
+
+            } as Community,
+         }))
+        
+        } catch (error) {
+            console.log("onUpdateImage Error", error)
+            
+        }
+        setUploadingImage(false)
+    }
+
     return (
         <Box position="sticky" top="14px">
             <Flex 
@@ -70,7 +106,7 @@ const About:React.FC<AboutProps> = ({communityData}) => {
                             </Text>
                         )}
                     </Flex>
-                    <Link href={`/r/${router.query.communityId}/submit`}>
+                    <Link href={`/r/${communityData.id}/submit`}>
                         <Button mt={3} height="30px">
                             Create Post
                         </Button>
@@ -85,12 +121,40 @@ const About:React.FC<AboutProps> = ({communityData}) => {
                                         color="blue.500" 
                                         cursor="pointer"
                                         _hover={{ textDecoration: "underline"}}
-                                        onClick={() => {}}
+                                        onClick={() => selectedFileRef.current?.click()}
                                         >
                                             Change Image
                                     </Text>
-                                    {}
+                                    {communityData.imageURL || selectedFile ? (
+                                        <Image 
+                                            src={selectedFile || communityData.imageURL}
+                                            borderRadius="full"
+                                            boxSize="40px"
+                                            alt="Community Image"
+                                        />
+                                    ): (
+                                        <Icon 
+                                            as={FaReddit}
+                                            fontSize={40}
+                                            color="brand.100"
+                                            mr={2}
+                                        />
+                                    )} 
                                 </Flex>
+                                {selectedFile && (
+                                    (uploadingImage ? (
+                                        <Spinner />
+                                    ): <Text cursor="pointer" onClick={onUpdateImage}>Save Changes</Text>
+                                    ))}
+                                    <input 
+                                        id="file-upload"
+                                        type="file" 
+                                        accept="image/x-png,image/gif,image/jpeg"
+                                        hidden
+                                        ref={selectedFileRef}
+                                        onChange={onSelectFile}
+                                    
+                                    />
                             </Stack>
                         </>
                     )}
--- frontend/src/components/Community/Header.tsx
+++ frontend/src/components/Community/Header.tsx
@@ -19,8 +19,17 @@ const Header:React.FC<HeaderProps> = ({ communityData}) => {
             <Box height="50%" bg="blue.500" />
             <Flex justify="center" bg="white" flexGrow={1}>
                 <Flex width="95%" maxWidth="860px">
-                    {communityData.imageURL ? (
-                        <Image />
+                    {communityStateValue.currentCommunity?.imageURL ? (
+                        <Image
+                            borderRadius="full"
+                            boxSize="66px" 
+                            src={communityStateValue.currentCommunity.imageURL}
+                            alt=""
+                            position="relative"
+                            top={-3}
+                            color="blue.500"
+                            border="4px solid white"    
+                        />
                     ):(
                     <Icon 
                         as={FaReddit} 
